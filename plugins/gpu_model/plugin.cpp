463cc357cc577d0629b0a61c406767836bf4f4a1
Prashanth Ganesh <prashanthcganesh108@illixr.CS.Berkeley.EDU>
2025-04-26 00:07:12 -0700

    .

--- plugins/gpu_model/plugin.cpp
+++ plugins/gpu_model/plugin.cpp
@@ -54,7 +54,7 @@ public:
 
         long int delay_ns = read_delay_time();
 
-        // std::cout << "[illixr guest] render delaying for: " << delay_ns << std::endl;
+        std::cout << "[illixr guest] render delaying for: " << delay_ns << std::endl;
         std::this_thread::sleep_for(std::chrono::nanoseconds(delay_ns));
 
     }
@@ -65,7 +65,7 @@ public:
 
         long int delay_ns = read_delay_time();
 
-        // std::cout << "[illixr guest] compute delaying for: " << delay_ns << std::endl;
+        std::cout << "[illixr guest] compute delaying for: " << delay_ns << std::endl;
         std::this_thread::sleep_for(std::chrono::nanoseconds(delay_ns));
     }
 
@@ -104,14 +104,14 @@ private:
     }
 
     long int read_delay_time() {
-        return 0;
+        // return 0;
         // look for one packet containing the amount of time to delay in ns
         while ((reg_read8(GRAPHICS_STATUS) & 0x1) == 0) ;
         return (long int) reg_read32(GRAPHICS_OUT);
     }
 
     void send_packets(uint32_t* packets, int len) {
-        return;
+        // return;
         std::unique_lock lock{bridge_mutex};
 
         // std::cout << "[illixr guest] sending packets: " << std::endl;;
