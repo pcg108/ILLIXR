16f6382b34e5c7d9f197493ece71ba7dc1e3fbff
Prashanth Ganesh <prashanthcganesh108@illixr.CS.Berkeley.EDU>
2025-04-26 14:54:28 -0700

    .

--- cmake/Findonnxruntime.cmake
+++ cmake/Findonnxruntime.cmake
@@ -1,4 +1,4 @@
-set(onnxruntime_INCLUDE_DIRS /usr/local/include/onnxruntime/include)
+set(onnxruntime_INCLUDE_DIRS /scratch/prashanth/illixr-deps/include/onnxruntime/include/)
 set(onnxruntime_LIBRARIES onnxruntime)
 message("onnxruntime configured")
 # set(onnxruntime_FOUND 1)
