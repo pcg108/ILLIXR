15fe27a0590dc126563f6366cec6d8e25a8aa663
Prashanth Ganesh <prashanthcganesh108@illixr.CS.Berkeley.EDU>
2025-04-30 15:10:14 -0700

    debugging xdma

--- plugins/eye_tracking_target/plugin.cpp
+++ plugins/eye_tracking_target/plugin.cpp
@@ -121,6 +121,8 @@ class eye_tracking_target_impl : public eye_tracking_target {
             int img_size = img.total() * img.elemSize(); 
             gpu->copy_to_dma(img.data, img_size);
 
+            print_first_20_rows(mat);
+
             // send bridge stream message to host illixr worker to read the image and run the model on host
             uint32_t response_buffer[3];
             gpu->send_gpu_compute_message(2, img_size, 3, response_buffer);
@@ -146,6 +148,13 @@ class eye_tracking_target_impl : public eye_tracking_target {
 
 
 private:
+
+void print_first_20_rows(const cv::Mat& mat) {
+    int rows_to_print = std::min(20, mat.rows);
+    cv::Mat first_rows = mat(cv::Range(0, rows_to_print), cv::Range::all());
+
+    std::cout << "First " << rows_to_print << " rows of matrix:\n" << first_rows << std::endl;
+}
 
     cv::Mat preprocess_img(cv::Mat img) {
          // preprocess the eye image for RITnet
