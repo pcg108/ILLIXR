df5b0d674711b6b18b25aa727e2c092e5afd9594
Prashanth Ganesh <prashanthcganesh108@illixr.CS.Berkeley.EDU>
2025-05-07 18:49:51 -0700

    .

--- cmake/GetGTSAM.cmake
+++ cmake/GetGTSAM.cmake
@@ -6,7 +6,7 @@ find_package(GTSAM 4.3.0 EXACT QUIET)
 if(NOT GTSAM_FOUND)
     EXTERNALPROJECT_ADD(GTSAM_EXT
             GIT_REPOSITORY https://github.com/pcg108/gtsam.git   # Git repo for source code
-            GIT_TAG f1bd826dc4d32dde43fa757bd72dce364b9ed382     # sha5 hash for specific commit to pull (if there is no specific tag to use)
+            GIT_TAG b5b744fe7a801f0862b21f53137adafc12862806     # sha5 hash for specific commit to pull (if there is no specific tag to use)
             PREFIX ${CMAKE_BINARY_DIR}/_deps/GTSAM               # the build directory
             # arguments to pass to CMake
             CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DCMAKE_BUILD_TYPE=Release -DGTSAM_WITH_TBB=OFF -DGTSAM_USE_SYSTEM_EIGEN=OFF -DGTSAM_POSE3_EXPMAP=ON -DGTSAM_ROT3_EXPMAP=ON -DGTSAM_BUILD_TESTS=OFF -DGTSAM_BUILD_EXAMPLES_ALWAYS=OFF -DCMAKE_INSTALL_LIBDIR=lib
