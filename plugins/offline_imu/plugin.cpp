34ba79ee5c85e1c426aa54e80055c8cdeb8459a6
Prashanth Ganesh <prashanthcganesh108@illixr.CS.Berkeley.EDU>
2025-05-13 13:45:53 -0700

    .

--- plugins/offline_imu/plugin.cpp
+++ plugins/offline_imu/plugin.cpp
@@ -49,8 +49,7 @@ protected:
         _m_imu.put(_m_imu.allocate<imu_type>(imu_type{real_now, (sensor_datum.imu0.angular_v), (sensor_datum.imu0.linear_a)}));
         ++_m_sensor_data_it;
 
-        time_point          imu_now(std::chrono::duration<long, std::nano>{dataset_now});
-        _m_imu_time.put(_m_imu_time.allocate<time_point>(imu_now));
+        _m_imu_time.put(_m_imu_time.allocate<time_point>(std::chrono::duration<long, std::nano>{dataset_now}));
     }
 
 private:
