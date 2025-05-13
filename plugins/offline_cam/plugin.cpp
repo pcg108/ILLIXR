766c9cc86e7f1f5bac3450f1f757fb868d1e1247
Prashanth Ganesh <prashanthcganesh108@illixr.CS.Berkeley.EDU>
2025-05-13 12:56:01 -0700

    .

--- plugins/offline_cam/plugin.cpp
+++ plugins/offline_cam/plugin.cpp
@@ -41,10 +41,14 @@ public:
             return;
         }
         duration time_since_start = imu_val->time.time_since_epoch();
+        
+        if (time_since_start < dataset_first_time) {
+            return;
+        }
 
         // duration begin            = time_since_start;
-        ullong lookup_time = std::chrono::nanoseconds{time_since_start}.count() + dataset_first_time;
-        std::cout << "[offline-cam] lookup time: " << lookup_time << std::endl;
+        ullong lookup_time = std::chrono::nanoseconds{time_since_start}.count(); //+ dataset_first_time;
+        // std::cout << "[offline-cam] lookup time: " << lookup_time << std::endl;
         std::map<ullong, sensor_types>::const_iterator nearest_row;
 
         // "std::map::upper_bound" returns an iterator to the first pair whose key is GREATER than the argument.
