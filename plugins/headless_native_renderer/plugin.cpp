3c0d79f4d577ade29cde14f7ea0261a8c7c83f30
Prashanth Ganesh <prashanthcganesh108@illixr.CS.Berkeley.EDU>
2025-05-15 00:14:12 -0700

    .

--- plugins/headless_native_renderer/plugin.cpp
+++ plugins/headless_native_renderer/plugin.cpp
@@ -74,7 +74,7 @@ public:
         uint64_t before_render_pose = rdcycle();
 
             auto render_pose = pp->get_fast_pose();
-            std::cout << "pose: " << render_pose.pose.position.x() << " " << render_pose.pose.position.y() << " " << render_pose.pose.position.z() << std::endl;
+            // std::cout << "pose: " << render_pose.pose.position.x() << " " << render_pose.pose.position.y() << " " << render_pose.pose.position.z() << std::endl;
 
         uint64_t after_render_pose = rdcycle();
 
@@ -97,7 +97,7 @@ public:
         uint64_t after_tw = rdcycle();
 
         // std::cout << "MTP: " << duration2double<std::milli>(_m_clock->now() - timewarp_pose.pose.sensor_time) << std::endl;
-        std::cout << "MTP: " << after_tw - before_render_pose << std::endl;
+        // std::cout << "MTP: " << after_tw - before_render_pose << std::endl;
 
         mtp_logger.log(record{mtp_record,
             {
