3914b4c6b345512070299b927a5148dc54633b7d
Prashanth Ganesh <prashanthcganesh108@illixr.CS.Berkeley.EDU>
2025-05-14 21:53:13 -0700

    .

--- cmake/GetOpenVINS.cmake
+++ cmake/GetOpenVINS.cmake
@@ -7,7 +7,7 @@ endif()
 
 EXTERNALPROJECT_ADD(OpenVINS
         GIT_REPOSITORY https://github.com/pcg108/open_vins.git   # Git repo for source code
-        GIT_TAG fabca64423edcb51e080964380d0481b275e391f         # sha5 hash for specific commit to pull (if there is no specific tag to use)
+        GIT_TAG 1e47e78a4a6220ab901153f4694a68d6f34f2dd8         # sha5 hash for specific commit to pull (if there is no specific tag to use)
         PREFIX ${CMAKE_BINARY_DIR}/_deps/OpenVINS                # the build directory
         DEPENDS ${OpenCV_DEP_STR}   # dependencies of this module
         #arguments to pass to CMake
