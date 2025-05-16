8321868a0ec5f9c3b523ee64e50deea6fcae9bdd
Prashanth Ganesh <prashanthcganesh108@illixr.CS.Berkeley.EDU>
2025-05-16 11:04:14 -0700

    .

--- plugins/gpu_model/plugin.cpp
+++ plugins/gpu_model/plugin.cpp
@@ -42,7 +42,7 @@ public:
     }
 
     void send_gpu_render_message(fast_pose_type current_pose, eye_position_type eye_pos, int queue_id, int read_dma_bytes, int num_response_expected, uint32_t* response_buffer) {
-        // std::cout << "[illixr guest] sending gpu message" << std::endl;
+        std::cout << "[illixr guest] sending gpu message" << std::endl;
 
         tx_packets[0] = make_start_packet(queue_id, 9, read_dma_bytes);
         make_pose_packets(tx_packets, current_pose.pose, 1);
@@ -54,10 +54,10 @@ public:
 
         for (int i = 0; i < num_response_expected; i++) {
             response_buffer[i] = read_packet();
-            // std::cout << " gpu model received: " << response_buffer[i] << std::endl;
+            std::cout << " gpu model received: " << response_buffer[i] << std::endl;
         }
 
-        // std::cout << "[illixr guest] render delaying for: " << response_buffer[0] << std::endl;
+        std::cout << "[illixr guest] render delaying for: " << response_buffer[0] << std::endl;
         std::this_thread::sleep_for(std::chrono::nanoseconds(response_buffer[0]));
 
     }
