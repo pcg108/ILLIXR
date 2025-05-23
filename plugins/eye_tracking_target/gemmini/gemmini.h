b6c31cd22be4ab08feb841a6d08bdaa108479887
Prashanth Ganesh <prashanthcganesh108@illixr.CS.Berkeley.EDU>
2025-05-23 11:24:15 -0700

    changing relu name in gemmini.h

--- plugins/eye_tracking_target/gemmini/gemmini.h
+++ plugins/eye_tracking_target/gemmini/gemmini.h
@@ -72,7 +72,7 @@
 #define WEIGHT_STATIONARY 1
 
 #define NO_ACTIVATION 0
-#define RELU 1
+#define RELU_ 1
 #define LAYERNORM 2
 #define IGELU 3
 #define SOFTMAX 4
@@ -890,7 +890,7 @@ static elem_t scale_and_sat(acc_t x, int act, acc_scale_t scale, acc_scale_t ber
   // Clip result
   x = x > elem_t_max ? elem_t_max : (x < elem_t_min ? elem_t_min : x);
   // Apply activation function
-  if (act == RELU) {
+  if (act == RELU_) {
     x = x < 0 ? 0 : x;
   }
   return x;
@@ -3223,7 +3223,7 @@ static void tiled_resadd(const size_t I, const size_t J,
         bool relu,
         enum tiled_matmul_type_t matadd_type) {
 
-    gemmini_extended_config_st(stride * sizeof(elem_t), relu ? RELU : NO_ACTIVATION, C_scale);
+    gemmini_extended_config_st(stride * sizeof(elem_t), relu ? RELU_ : NO_ACTIVATION, C_scale);
     gemmini_config_ex(WS, 0, 0);
 
     gemmini_extended4_config_ld(stride * sizeof(elem_t), A_scale, true, DIM, 0);
