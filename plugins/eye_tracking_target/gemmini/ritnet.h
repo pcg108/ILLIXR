2301543839bf549da2431200ca8897750841ce52
Prashanth Ganesh <prashanthcganesh108@illixr.CS.Berkeley.EDU>
2025-05-23 20:48:50 -0700

    fixing signatures:

--- plugins/eye_tracking_target/gemmini/ritnet.h
+++ plugins/eye_tracking_target/gemmini/ritnet.h
@@ -7,7 +7,7 @@ extern "C" {
 
 #include <stdint.h>
 
-void gemmini_inference(int8_t * image_input, float eye_x, float eye_y); // put actual signature here
+void gemmini_inference(int8_t image_input[1][160][240][1], float eye_x, float eye_y); // put actual signature here
 
 #ifdef __cplusplus
 }
