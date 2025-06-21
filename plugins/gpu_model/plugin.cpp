0b59a05321b34eaa211d481755c92caa80cfdd2f
Prashanth Ganesh <prashanthcganesh108@illixr.CS.Berkeley.EDU>
2025-06-21 12:52:03 -0700

    .

--- plugins/gpu_model/plugin.cpp
+++ plugins/gpu_model/plugin.cpp
@@ -153,13 +153,13 @@ private:
         // return;
         std::unique_lock lock{bridge_mutex};
 
-        // std::cout << "[illixr guest] sending packets: " << std::endl;;
+        std::cout << "[illixr guest] sending packets: " << std::endl;;
         for (int i = 0; i < len; i++) {
-            // std::cout << "   " << packets[i] << std::endl;
+            std::cout << "   " << packets[i] << std::endl;
             while ((reg_read8(GRAPHICS_STATUS) & 0x2) == 0) ;
             reg_write32(GRAPHICS_IN, packets[i]);
         }
-        // std::cout << "[illixr guest] finished sending packets" << std::endl;
+        std::cout << "[illixr guest] finished sending packets" << std::endl;
     }
 
     uint32_t float_to_uint32(float val) {
