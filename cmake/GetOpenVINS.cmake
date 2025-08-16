dfde701883f158d4550a85dfbd0c71cd09a5c0a5
Prashanth Ganesh <prashanthcganesh108@illixr.CS.Berkeley.EDU>
2025-08-15 21:05:53 -0700

    removing openvins submodules for eigen and openblas

--- cmake/GetOpenVINS.cmake
+++ cmake/GetOpenVINS.cmake
@@ -7,7 +7,7 @@ endif()
 
 EXTERNALPROJECT_ADD(OpenVINS
         GIT_REPOSITORY https://github.com/pcg108/open_vins.git   # Git repo for source code
-        GIT_TAG 0bd663a96fcdd5bb9724ac2cf42298a814d351fd         # sha5 hash for specific commit to pull (if there is no specific tag to use)
+        GIT_TAG de3769156bbce3d4945c477af90b7eceb4d665db         # sha5 hash for specific commit to pull (if there is no specific tag to use)
         PREFIX ${CMAKE_BINARY_DIR}/_deps/OpenVINS                # the build directory
         DEPENDS ${OpenCV_DEP_STR}   # dependencies of this module
         #arguments to pass to CMake
