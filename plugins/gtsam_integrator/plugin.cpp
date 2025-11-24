1332cce2115534965fac66caa9f0ff5342769d72
pcg108 <prashanthcg108@gmail.com>
2025-11-24 13:51:10 -0800

    Disable debug logs for pose and IMU integration
    Comment out debug logging for pose integration.

--- plugins/gtsam_integrator/plugin.cpp
+++ plugins/gtsam_integrator/plugin.cpp
@@ -183,7 +183,7 @@ private:
 
 #ifndef NDEBUG
         if (input_values->last_cam_integration_time > last_cam_time) {
-            spdlog::get(name)->debug("New slow pose has arrived!");
+            // spdlog::get(name)->debug("New slow pose has arrived!");
             last_cam_time = input_values->last_cam_integration_time;
         }
 #endif
@@ -219,7 +219,7 @@ private:
         ImuBias bias      = _pim_obj->biasHat();
 
 #ifndef NDEBUG
-        spdlog::get(name)->debug("Integrating over {} IMU samples", prop_data.size());
+        // spdlog::get(name)->debug("Integrating over {} IMU samples", prop_data.size());
 #endif
 
         for (std::size_t i = 0; i < prop_data.size() - 1; i++) {
@@ -233,9 +233,9 @@ private:
         gtsam::Pose3    out_pose   = navstate_k.pose();
 
 #ifndef NDEBUG
-        spdlog::get(name)->debug("Base Position (x, y, z) = {}, {}, {}", input_values->position(0), input_values->position(1),
-                                 input_values->position(2));
-        spdlog::get(name)->debug("New Position (x, y, z) = {}, {}, {}", out_pose.x(), out_pose.y(), out_pose.z());
+        // spdlog::get(name)->debug("Base Position (x, y, z) = {}, {}, {}", input_values->position(0), input_values->position(1),
+                                 // input_values->position(2));
+        // spdlog::get(name)->debug("New Position (x, y, z) = {}, {}, {}", out_pose.x(), out_pose.y(), out_pose.z());
 #endif
 
         auto                        seconds_since_epoch = std::chrono::duration<double>(real_time.time_since_epoch()).count();
